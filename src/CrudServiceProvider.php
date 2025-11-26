b9abed0c1e5ba5737a575d9fef3e59208de8dc10
Adith Widhiantara <aditya.saktyawan@javan.co.id>
2025-11-26 14:33:00 +0700

    Initiate CRUD Package

---
+++ src/CrudServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Adithwidhiantara\Crud;
+
+use Illuminate\Support\ServiceProvider;
+
+class CrudServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        // dd('works');
+    }
+}
